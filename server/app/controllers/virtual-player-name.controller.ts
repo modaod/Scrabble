a9c8c810fcc8698d5c384a467d33bc6e5d2780c4
Moussa <moussa-abdillahi.daoud@polymtl.ca>
2022-02-07 15:07:27 -0500

    virtual player name backend updated

--- server/app/controllers/virtual-player-name.controller.ts
+++ server/app/controllers/virtual-player-name.controller.ts
@@ -1,7 +1,6 @@
-import { ELEMENT_NOT_FOUNDED, HHTP_SUCCESSFUL_REQUEST, HTTP_STATUS_CREATED } from '@app/constants/constant';
-import { DatabaseService } from '@app/services/database.service';
-import { Request, Response, Router } from 'express';
 import { Service } from 'typedi';
+import { Request, Response, Router } from 'express';
+import { ELEMENT_NOT_FOUNDED, HHTP_SUCCESSFUL_REQUEST, HTTP_STATUS_CREATED } from '@app/constants/constant';
 
 @Service()
 export class VirtualPlayerNameController {
@@ -10,7 +9,7 @@ export class VirtualPlayerNameController {
     advancedPlayername: string[] = ['James', 'Sanmar', 'Marc'];
     beginnerPlayerName: string[] = ['Simon', 'Jack', 'Ali'];
 
-    constructor(private database: DatabaseService) {
+    constructor() {
         this.configureRouter();
     }
 
@@ -18,18 +17,22 @@ export class VirtualPlayerNameController {
         this.router = Router();
 
         this.router.post('/', (req: Request, res: Response) => {
-            this.database.addNewVirtualPlayerName(req.body.name, req.body.category);
+            if (req.body.category === 'advanced') this.advancedPlayername.push(req.body.name);
+            else this.beginnerPlayerName.push(req.body.name);
+
             res.status(HTTP_STATUS_CREATED).json({ message: 'Nom enregistré !' });
         });
 
         this.router.put('/:id', (req: Request, res: Response) => {
-            const name = req.params.id;
-            this.database.modifyVirtualPlayerName(req.body.newName, name, req.body.category);
+            const index: number = +req.params.id;
+            if (req.body.category === 'advanced') this.advancedPlayername[index] = req.body.newName;
+            else this.beginnerPlayerName[index] = req.body.newName;
+
             res.status(HHTP_SUCCESSFUL_REQUEST).json({ message: 'Element modifié !' });
         });
 
         this.router.delete('/:id', (req: Request, res: Response) => {
-            const index = this.advancedPlayername.indexOf(req.body, 0);
+            const index = this.advancedPlayername.indexOf(req.params.id, 0);
             if (index > ELEMENT_NOT_FOUNDED) {
                 this.advancedPlayername.splice(index, 1);
             } else this.beginnerPlayerName.splice(index, 1);
